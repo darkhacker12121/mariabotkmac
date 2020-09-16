0a31e12a1b1cc649bfafd1b8a8d13d6c36f43d9f
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2020-09-16 21:26:53 +0530

    Update notes.py

--- tg_bot/modules/notes.py
+++ tg_bot/modules/notes.py
@@ -60,9 +60,9 @@ def get(bot, update, notename, show_none=True, no_format=False):
                 try:
                     bot.forward_message(chat_id=chat_id, from_chat_id=MESSAGE_DUMP, message_id=note.value)
                 except BadRequest as excp:
-                    if excp.message == "Message to forward not found":
-                        message.reply_text("This message seems to have been lost - I'll remove it "
-                                           "from your notes list.")
+                    if excp.message == "ඉදිරියට යැවීමේ පණිවිඩය හමු නොවීය":
+                        message.reply_text("Tඔහුගේ පණිවිඩය නැති වී ඇති බව පෙනේ - මම එය ඉවත් කරමි"
+                                           "ඔබගේ සටහන් ලැයිස්තුවෙන්.")
                         sql.rm_note(chat_id, notename)
                     else:
                         raise
@@ -70,11 +70,11 @@ def get(bot, update, notename, show_none=True, no_format=False):
                 try:
                     bot.forward_message(chat_id=chat_id, from_chat_id=chat_id, message_id=note.value)
                 except BadRequest as excp:
-                    if excp.message == "Message to forward not found":
-                        message.reply_text("Looks like the original sender of this note has deleted "
-                                           "their message - sorry! Get your bot admin to start using a "
-                                           "message dump to avoid this. I'll remove this note from "
-                                           "your saved notes.")
+                    if excp.message == "ඉදිරියට යැවීමේ පණිවිඩය හමු නොවීය":
+                        message.reply_text("මෙම සටහනේ මුල් යවන්නා මකා දමා ඇති බව පෙනේ"
+                                           "ඔවුන්ගේ පණිවිඩය - සමාවෙන්න! භාවිතා කිරීම ආරම්භ කිරීමට ඔබේ බොට් පරිපාලක ලබා ගන්න "
+                                           "මෙය වළක්වා ගැනීම සඳහා පණිවිඩ ඩම්ප් කරන්න. මම මෙම සටහන ඉවත් කරමි "
+                                           "ඔබගේ සුරකින ලද සටහන්.")
                         sql.rm_note(chat_id, notename)
                     else:
                         raise
@@ -105,23 +105,23 @@ def get(bot, update, notename, show_none=True, no_format=False):
                                                 reply_markup=keyboard)
 
             except BadRequest as excp:
-                if excp.message == "Entity_mention_user_invalid":
-                    message.reply_text("Looks like you tried to mention someone I've never seen before. If you really "
-                                       "want to mention them, forward one of their messages to me, and I'll be able "
-                                       "to tag them!")
+                if excp.message == "ආයතනය_සඳහන්_කිරීම_පරිශීලකයා_අවලංගුය":
+                    message.reply_text("මම මීට පෙර කවදාවත් දැක නැති කෙනෙකු ගැන සඳහන් කිරීමට ඔබ උත්සාහ කළ බවක් පෙනේ. ඔබ ඇත්තටම නම් "
+                                       "ඒවා සඳහන් කිරීමට අවශ්‍යයි, ඔවුන්ගේ පණිවිඩයක් මා වෙත යොමු කරන්න, එවිට මට හැකි වනු ඇත"
+                                       "ඒවා ටැග් කිරීමට!")
                 elif FILE_MATCHER.match(note.value):
-                    message.reply_text("This note was an incorrectly imported file from another bot - I can't use "
-                                       "it. If you really need it, you'll have to save it again. In "
-                                       "the meantime, I'll remove it from your notes list.")
+                    message.reply_text("මෙම සටහන වෙනත් බොට් එකකින් වැරදි ලෙස ආනයනය කරන ලද ගොනුවකි - මට භාවිතා කළ නොහැක "
+                                       "එය. ඔබට එය සැබවින්ම අවශ්‍ය නම්, ඔබට එය නැවත සුරැකීමට සිදුවේ. තුළ "
+                                       "මේ අතර, මම එය ඔබගේ සටහන් ලැයිස්තුවෙන් ඉවත් කරමි.")
                     sql.rm_note(chat_id, notename)
                 else:
-                    message.reply_text("This note could not be sent, as it is incorrectly formatted. Ask in "
-                                       "@MarieSupport if you can't figure out why!")
-                    LOGGER.exception("Could not parse message #%s in chat %s", notename, str(chat_id))
+                    message.reply_text("මෙම සටහන වැරදි ලෙස සංයුති කර ඇති බැවින් එය යැවිය නොහැක. ඇතුලට අහන්න"
+                                       "@cyberwordk ඇයි කියලා හිතාගන්න බැරි නම්!")
+                    LOGGER.exception("පණිවිඩය විග්‍රහ කිරීමට නොහැකි විය #%s කතාබස් කරමින් %s", notename, str(chat_id))
                     LOGGER.warning("Message was: %s", str(note.value))
         return
     elif show_none:
-        message.reply_text("This note doesn't exist")
+        message.reply_text("මෙම සටහන නොපවතී")
 
 
 @run_async
@@ -172,19 +172,19 @@ def save(bot: Bot, update: Update):
     sql.add_note_to_db(chat_id, note_name, text, data_type, buttons=buttons, file=content)
 
     msg.reply_text(
-        "OK, Added {note_name} in *{chat_name}*.\nGet it with /get {note_name}, or #{note_name}".format(note_name=note_name, chat_name=chat_name), parse_mode=ParseMode.MARKDOWN)
+        "හරි, එකතු කළා {note_name} in *{chat_name}*.\nGet it with /get {note_name}, or #{note_name}".format(note_name=note_name, chat_name=chat_name), parse_mode=ParseMode.MARKDOWN)
 
     if msg.reply_to_message and msg.reply_to_message.from_user.is_bot:
         if text:
-            msg.reply_text("Seems like you're trying to save a message from a bot. Unfortunately, "
-                           "bots can't forward bot messages, so I can't save the exact message. "
-                           "\nI'll save all the text I can, but if you want more, you'll have to "
-                           "forward the message yourself, and then save it.")
+            msg.reply_text("ඔබ බොට් එකකින් පණිවිඩයක් සුරැකීමට උත්සාහ කරන බවක් පෙනේ. අවාසනාවට, "
+                           "බොට්ස් වලට බොට් පණිවිඩ යැවිය නොහැක, එබැවින් මට නිශ්චිත පණිවිඩය සුරැකිය නොහැක. "
+                           "\nමට හැකි සියලුම පෙළ මම සුරකිමි, නමුත් ඔබට තවත් අවශ්‍ය නම් ඔබට එය කිරීමට සිදුවේ "
+                           "පණිවිඩය ඔබම යොමු කර එය සුරකින්න.")
         else:
-            msg.reply_text("Bots are kinda handicapped by telegram, making it hard for bots to "
-                           "interact with other bots, so I can't save this message "
-                           "like I usually would - do you mind forwarding it and "
-                           "then saving that new message? Thanks!")
+            msg.reply_text("බොට්ස් ටෙලිග්‍රාම් මගින් තරමක් ආබාධිත වන අතර එය බොට් කිරීමට අපහසු වේ"
+                           "වෙනත් බොට්ස් සමඟ අන්තර් ක්‍රියා කරන්න, එවිට මට මෙම පණිවිඩය සුරැකිය නොහැක "
+                           "මම සාමාන්‍යයෙන් කැමතියි - ඔබට එය ඉදිරිපත් කිරීමට අවශ්‍යද?"
+                           "එහෙනම් ඒ නව පණිවිඩය සුරකිනවාද? ස්තූතියි!")
         return
 
 
@@ -208,9 +208,9 @@ def clear(bot: Bot, update: Update, args: List[str]):
         notename = args[0]
 
         if sql.rm_note(chat_id, notename):
-            update.effective_message.reply_text("Successfully removed note.")
+            update.effective_message.reply_text("සටහන සාර්ථකව ඉවත් කරන ලදි.")
         else:
-            update.effective_message.reply_text("That's not a note in my database!")
+            update.effective_message.reply_text("එය මගේ දත්ත ගබඩාවේ සටහනක් නොවේ!")
 
 
 @run_async
@@ -265,9 +265,9 @@ def __import_data__(chat_id, data):
         with BytesIO(str.encode("\n".join(failures))) as output:
             output.name = "failed_imports.txt"
             dispatcher.bot.send_document(chat_id, document=output, filename="failed_imports.txt",
-                                         caption="These files/photos failed to import due to originating "
-                                                 "from another bot. This is a telegram API restriction, and can't "
-                                                 "be avoided. Sorry for the inconvenience!")
+                                         caption="මේ files/photos ආරම්භය හේතුවෙන් ආනයනය කිරීමට අපොහොසත් විය "
+                                                 "වෙනත් බෝට්ටුවකින්. මෙය විදුලි පණිවුඩ API සීමා කිරීමක් වන අතර එය කළ නොහැක "
+                                                 "වළක්වා ගත යුතුය. අපහසුතාවයට කනගාටුයි!")
 
 
 def __stats__():
@@ -284,19 +284,19 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- - /get <notename>: get the note with this notename
+ - /get <notename>: මෙම සටහන් නාමය සමඟ සටහන ලබා ගන්න
  - #<notename>: same as /get
- - /notes or /saved: list all saved notes in this chat
+ - /notes or /saved:මෙම සංවාදයේ සුරකින ලද සියලුම සටහන් ලැයිස්තුගත කරන්න
 
-If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
-be useful when updating a current note.
+සටහනක අන්තර්ගතය කිසිදු හැඩතල ගැන්වීමකින් තොරව ලබා ගැනීමට ඔබ කැමති නම්, භාවිතා කරන්න`/get <notename> noformat`.මෙය කළ හැකිය \
+වත්මන් සටහනක් යාවත්කාලීන කිරීමේදී ප්‍රයෝජනවත් වන්න.
 
-*Admin only:*
- - /save <notename> <notedata>: saves notedata as a note with name notename
-A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
-`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check /markdownhelp for more info.
- - /save <notename>: save the replied message as a note with name notename
- - /clear <notename>: clear note with this name
+*පරිපාලක පමණි:*
+ - /save <notename> <notedata>:සටහන් නාම නාමයක් සහිත සටහනක් ලෙස සටහන් කිරීම සුරකිනු ඇත
+සම්මත සලකුණු සම්බන්ධක සින්ටැක්ස් භාවිතා කිරීමෙන් සටහනකට බොත්තමක් එක් කළ හැකිය - සබැඳිය a සමඟ පෙර සූදානම් කළ යුතුය\
+`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check /markdownhelp වැඩි විස්තර සඳහා.
+ - /save <notename>: පිළිතුරු දුන් පණිවිඩය නාම නාමයක් සහිත සටහනක් ලෙස සුරකින්න
+ - /clear <notename>: මෙම නම සහිත පැහැදිලි සටහනක්
 """
 
 __mod_name__ = "Notes"
